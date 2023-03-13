331c4bb396cf8296307b16969bfc958f979518ef
rozze21 <tampasort@hotmail.com>
2023-03-13 08:35:27 -0700

    initial imiport

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # cohort-02-2023-java-wk1
+
+##Stuff to know
+
+This is a bunch of stuff to know
+1. Install Eclipse
+1. Install Git client
+1. Have aspirin handy!!
---
+++ src/week/one/Week1DemoOfStuff.java
@@ -0,0 +1,28 @@
+package week.one;
+
+public class Week1DemoOfStuff {
+
+  /**
+   * This is the entry point for Java Application. This is an additional comment on the line
+   */
+
+  public static void main(String[] args) {
+    
+    // TODO Auto-generated method stub
+    double burgerCost = 13.45;
+    double cashInWallet = 40.00;
+    
+    System.out.printf(
+        "I can buy %.2f burgers!%n", cashInWallet / burgerCost);
+    
+    
+    /**
+    System.out.println("I can buy " + cashInWallet / burgerCost + " burgers!");
+    %d
+    /**
+    System.out.println(3 + 4);
+    System.out.println("3"+"4" +5);
+    */
+  }
+
+}
